362c717009b8040c1556422e6e050b26cc409a02
KOITT <KOITT@KOITT-PC03>
2018-02-07 17:50:01 +0900

    게시판만들기 수정 보완 board-users연결

--- KoittBoardServletJSP/sql/board.sql
+++ KoittBoardServletJSP/sql/board.sql
@@ -7,18 +7,18 @@ CREATE TABLE board (
 	content VARCHAR(255) NULL,
 	user_no INT NOT NULL,
 	regdate DATE NOT NULL,
-	FOREIGN KEY (user_no) REFERENCES users (no)
+	FOREIGN KEY (user_no) REFERENCES users(no)
 );
 
 #DML(Data Manipulation Language)
 INSERT INTO board (title, content, writer, regdate)
-	VALUES ('제목-1', '내용-1', 1, STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
+	VALUES ('제목-1', '내용-1', '작성자-1', STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
 	
 INSERT INTO board (title, content, writer, regdate)
-	VALUES ('제목-2', '내용-2', 2, STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
+	VALUES ('제목-2', '내용-2', '작성자-2', STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
 	
 INSERT INTO board (title, content, writer, regdate)
-	VALUES ('제목-3', '내용-3', 3, STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
+	VALUES ('제목-3', '내용-3', '작성자-3', STR_TO_DATE('2018-02-01', '%Y-%m-%d'));
 	
 #DQL(Data Query Language)
 SELECT * FROM board;
--- KoittBoardServletJSP/sql/users-board.sql
+++ KoittBoardServletJSP/sql/users-board.sql
@@ -1,20 +1,16 @@
-
 INSERT INTO users (email, password, name)
-	VALUES ('m_@_m.com', '1234', '슈퍼맨');
-	
-INSERT INTO users (email, password, name)
-	VALUES ('o_@-.com', '1234', '땡글이');
-	
-INSERT INTO users (email, password, name)
-	VALUES ('m_@_j.com', '1234', '미지');
+	VALUES ('apple@koitt.com', '1234', '김사과');
 	
 SELECT * FROM users ORDER BY no DESC;
 
-INSERT INTO board (title, content, user_no, regdate)
-	VALUES ('안녕', '안녕하세여', 15, CURDATE());
+INSERT INTO board (title, content, user_no, regdate) 
+	VALUES ('사과-1', '사과내용-1', 8, CURDATE());
+	
+INSERT INTO board (title, content, user_no, regdate) 
+	VALUES ('사과-2', '사과내용-2', 8, CURDATE());
 	
 SELECT * FROM board ORDER BY no DESC;
 
-SELECT b.no, b.title, b.content, u.email, b.regdate 
-FROM board b, users u 
+SELECT b.no, b.title, b.content, u.email, b.regdate
+FROM board b, users u
 WHERE b.user_no = u.no;
